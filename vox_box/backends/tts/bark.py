5c4833b6ac7a92f931e6d9e4f5bdf32823460f94
michelia <michelia@seal.io>
2024-11-28 15:43:46 +0800

    fix: update bark

--- vox_box/backends/tts/bark.py
+++ vox_box/backends/tts/bark.py
@@ -73,7 +73,7 @@ class Bark(TTSBackend):
             raise ValueError(f"Voice {voice} not supported")
 
         inputs = self._processor(input, voice_preset=voice).to(self._cfg.device)
-        audio_array = self._model.generate(**inputs, history_prompt=voice)
+        audio_array = self._model.generate(**inputs)
         audio_array = audio_array.cpu().numpy().squeeze()
         sample_rate = self._model.generation_config.sample_rate
 
